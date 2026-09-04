8a912b000ac851dce3b67f305728dffed2d18313
Bandana210 <bandanajain0210@gmail.com>
2026-09-04 19:51:54 +0530

    made notebooks for models and EDA

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+def get_requirements(file_path:str)->list:
+    '''
+    This function will return the list of requirements listed in req.txt file 
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if '-e.' in requirements:
+            requirements.remove('-e.')
+
+
+    return requirements
+
+setup(
+name ='mlproject',
+version ='0.0.1',
+author = 'Bandana',
+author_email = 'bandanajain0210@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
